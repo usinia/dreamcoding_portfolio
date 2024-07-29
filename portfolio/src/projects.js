3df018d69f3a026d850ecf2c9f98a0b3da6087b7
김태림 <usinia@nexon.co.kr>
2024-07-29 16:01:15 +0900

    js - filter work

--- portfolio/src/projects.js
+++ portfolio/src/projects.js
@@ -1,1 +1,44 @@
 "use strict";
+
+// 프로젝트 필터링 관련 로직 처리
+const categoriesEl = document.querySelector(".categories");
+const projectsEl = document.querySelector(".projects");
+
+categoriesEl.addEventListener("click", (e) => {
+  const { category } = e.target.dataset;
+  if (!category) return;
+
+  /*
+  categoriesEl.querySelectorAll("button").forEach((v) => {
+    v.dataset.category === category
+      ? v.classList.add("category--selected")
+      : v.classList.remove("category--selected");
+  });
+  */
+  //  Active 메뉴를 재설정
+  const activeBtn = document.querySelector(".category--selected");
+  activeBtn.classList.remove("category--selected");
+  e.target.classList.add("category--selected");
+
+  projectsEl.querySelectorAll("li").forEach((v) => {
+    /*
+    if (category === "all") {
+      v.classList.remove("invisible");
+    } else {
+      if (v.dataset.category !== category) {
+        v.classList.add("invisible");
+      } else {
+        v.classList.remove("invisible");
+      }
+    }
+    */
+    //  프로젝트 필터링
+    if (category === "all" || v.dataset.category === category) {
+      // v.classList.remove("invisible");
+      v.style.display = "block";
+    } else {
+      // v.classList.add("invisible");
+      v.style.display = "none";
+    }
+  });
+});
